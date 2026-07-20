666c55dc11dac9a0a831834858b6cc62cecd70b6
Emilianodamb <emilianolrc@gmail.com>
2026-07-20 19:15:30 -0300

    First files

---
+++ TestProject/Program.cs
@@ -0,0 +1,107 @@
+﻿/*
+/// Llamar a métodos desde la biblioteca de clases .NET mediante C#
+
+//--- Unidad 4/8
+Random dice = new Random();
+
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
+//--- Unidad 5/8
+Random rnd = new();
+
+int firstValue = rnd.Next(100);
+int secondValue = rnd.Next(150);
+int largerValue;
+
+largerValue = System.Math.Max(firstValue, secondValue);
+//Console.WriteLine(largerValue);
+Console.WriteLine($"Se pidieron dos valores: {firstValue} y {secondValue}\nEl mayor de los dos es: {largerValue}");
+
+
+/// Adición de lógica de decisión al código mediante instrucciones 
+/// "if", "else" y "else if" en C#.
+
+//--- Unidad 2/7
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6; 
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Sorry, you loose.");
+}
+
+//--- Unidad 3/7
+*/
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus to total!");
+        total += 2;
+    }
+    Console.WriteLine($"Your total including the bonus is {total}");
+}
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if(total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if(total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a Kitten!");
+}
